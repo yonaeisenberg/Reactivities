df19958b2036e05453e636449fb8f6d96880cd99
yona <yonaei@essence-grp.com>
2022-07-12 15:04:33 +0300

    End of section 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,28 +1,49 @@
-import React, { Fragment, useEffect} from 'react';
+import React, { Fragment} from 'react';
 import { Container} from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
+
 import { observer } from 'mobx-react-lite';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
-  const {activityStore} = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore])
-
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
+  const location = useLocation();
 
   return (
-    <Fragment>
-      <NavBar />
-      <Container style={{marginTop: '7em'}}>   
-          <ActivityDashboard />
+    <>
+      <Container style={{marginTop: '7em'}}>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/activities' element={
+            <>
+              <NavBar />
+              <ActivityDashboard />
+            </>
+          } />
+          <Route path='/activities/:id' element={
+            <>
+              <NavBar />
+              <ActivityDetails />
+            </>
+          } />
+          <Route key={location.key} path='/createActivity' element={
+            <>
+              <NavBar />
+              <ActivityForm />
+            </>
+          } />
+          <Route key={location.key} path='/manage/:id' element={
+            <>
+              <NavBar />
+              <ActivityForm />
+            </>
+          } />
+        </Routes>
       </Container> 
-    </Fragment>
+    </>
   );
 }
 
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,18 @@
 import { observer } from "mobx-react-lite";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { Button, Form, Segment } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
+import {v4 as uuid} from 'uuid';
 
 export default observer(function ActivityForm() {
+    const navigate = useNavigate();
     const {activityStore} = useStore();
-    const {selectedActivity, closeForm, createActivity, updateActivity, loading} = activityStore;
+    const {createActivity, updateActivity, loading, loadActivity, loadingInitial, setLoadingInitial} = activityStore;
+    const {id} = useParams<{id: string}>();
 
-    const initialState = selectedActivity ?? {
+    const [activity, setActivity] = useState({
         id : '',
         title: '',
         category: '',
@@ -15,18 +20,44 @@ export default observer(function ActivityForm() {
         date: '',
         city: '',
         venue: ''
-    }
+    });
 
-    const [activity, setActivity] = useState(initialState);
+    useEffect (() => {
+        if (id) {
+            loadActivity(id).then(activity => setActivity(activity!));
+        }
+        else {
+            setActivity({
+                id : '',
+                title: '',
+                category: '',
+                description: '',
+                date: '',
+                city: '',
+                venue: ''
+            })
+        }
+        setLoadingInitial(false);
+    }, [id, loadActivity, setLoadingInitial]);
 
     function handleSubmit() {
-        activity.id ? updateActivity(activity) : createActivity(activity);
+        if (activity.id.length === 0){
+            let newActivity = {
+                ...activity,
+                id: uuid()
+            };
+            createActivity(newActivity).then(() => navigate(`/activities/${newActivity.id}`));
+        } else {
+         updateActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+        }
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
         const {name, value} = event.target;
         setActivity({...activity, [name]: value})
     }
+
+    if (loadingInitial) return <LoadingComponent content='Loading activity...'/>
 
     return(
         <Segment clearing>
@@ -38,7 +69,7 @@ export default observer(function ActivityForm() {
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange}/>
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
                 <Button loading={loading} floated='right' positive type='submit' content='Submit' />
-                <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
+                <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
             </Form>
         </Segment>
     )
